f5a7dd486db94354772fbe12249ca5cfa3ee5f86
NaritaKashin <71251802+NaritaKashin@users.noreply.github.com>
2020-11-18 15:29:34 +0900

    Update main_queue.py

--- main_queue.py
+++ main_queue.py
@@ -6,9 +6,15 @@ for html in htmls:
     res = requests.get(html)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('h2')
+    for script in soup(["script","style"]):
+        script.decompose()
+    text = soup.get_text()
+    lines = [line.strip() for line in text.splitlines()]
+    text="\n".join(line for line in lines if line)
+    oks = []
+    oks = text.splitlines()
     with open(f'{html_md5}.txt','a',encoding="utf-8") as f:
-        for elem in elems:
-            print(elem.get_text(),file=f)
+        for ok in oks:
+            print(ok,file=f)
         f.flush()
         os.fsync(f.fileno())

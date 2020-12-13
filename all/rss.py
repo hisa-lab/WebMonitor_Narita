084ae84f586a121508908a51db7de52d2791af83
NaritaKashin <71251802+NaritaKashin@users.noreply.github.com>
2020-12-13 16:10:52 +0900

    Add files via upload

---
+++ all/rss.py
@@ -0,0 +1,19 @@
+import requests, bs4, os, hashlib, json, datetime, feedparser
+
+with open('RSSURL.txt','r',encoding="utf-8") as f:
+    htmls = f.read().splitlines()
+
+for html in htmls:
+    fdp = feedparser.parse(html)
+    rawhtml = html.replace('/','').replace(':','').replace('https','').replace('http','')
+    jexport = {"url":"","date":"","contents":""}
+    jt = []
+    with open('rss/'f'{rawhtml}''.json','w',encoding="utf-8") as f:    
+        for entry in fdp.entries:
+            jexport["url"] = f"{entry.link}"
+            jexport["date"] = f"{entry.date}"
+            jexport["contents"] = f"{entry.title}"
+            jt.append(jexport)
+        json.dump(jt,f,indent=2,ensure_ascii=False)
+
+
